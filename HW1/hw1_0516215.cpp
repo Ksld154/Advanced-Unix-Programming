e335f240c3f1772d5a22c152817eb858921ea74b
Alex Lin <h427m4nn.cs05@g2.nctu.edu.tw>
2020-03-20 23:19:06 +0800

    [HW1] Initial List done, need to do ip format and conntype sort

--- HW1/hw1_0516215.cpp
+++ HW1/hw1_0516215.cpp
@@ -13,24 +13,71 @@ const string NET_FILE = "/proc/net/";
 
 
 struct connInfo {
-    char* localAddr;
-    char* remoteAddr;
-    char* inode;
+    int connType;
+    string localAddr;
+    string remoteAddr;
+    string inode;
 };
 
+// struct procInfo {
+//     const char* pid;
+//     const char* procName;
+//     const char* inode; 
+// };
+
 struct procInfo {
-    const char* pid;
-    const char* procName;
-    const char* inode; 
+    string pid;
+    string procName;
+    string inode; 
 };
 
 vector <struct connInfo> connList;
 vector <struct procInfo> inodeList;
 
-int parseConnEntry(char *);
-string parseProcName(struct procInfo inodeEntry);
+// struct ConnInfo parseConnEntry(char *, int);
+string parseProcName(struct procInfo);
+
+struct connInfo parseConnEntry(char *line, int connType){
+
+    struct connInfo conn;
+    conn.connType = connType;
+
+    int idx = 0;
+    char* substr = strtok(line, " ");
+    while(substr != NULL){
+        if(idx == 1){
+            conn.localAddr = substr;
+        }
+        else if(idx == 2){
+            conn.remoteAddr = substr;
+        }
+        else if(idx == 9){
+            conn.inode = substr;
+        }
+        substr = strtok(NULL, " ");
+        idx++;
+    }
+
+    printf("%s %s %s\n", conn.localAddr.c_str(), conn.remoteAddr.c_str(), conn.inode.c_str());
+    // connList.push_back(conn);
+    return conn;
+}
 
 int readConnection(string connType, const string NET_FILE){
+
+    int conn = -1;
+    if(connType == "tcp"){
+        conn = 0;
+    }
+    else if(connType == "tcp6"){
+        conn = 1;
+    }
+    else if(connType == "udp"){
+        conn = 2;
+    }
+    else if(connType == "udp6"){
+        conn = 3;
+    }
 
     const string connPath = NET_FILE + connType;
     printf("%s\n", connPath.c_str());
@@ -38,16 +85,17 @@ int readConnection(string connType, const string NET_FILE){
     FILE    *fp;
     char    *line = NULL;
     size_t  n = 0; 
-
     fp = fopen(connPath.c_str(), "r");
     if (fp == NULL){
-        // exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     int idx = 0;
     while(getline(&line, &n, fp) != -1){
+        struct connInfo connEntry;
         if (idx >= 1){
-            parseConnEntry(line);
+            connEntry = parseConnEntry(line, conn);
+            connList.push_back(connEntry);
         }
         idx++;
     }
@@ -70,13 +118,11 @@ int scanProcess(){
         // exit(EXIT_FAILURE);
     }
 
-
     struct dirent *dirent_ptr; readdir(dir_ptr);  
     int dfd = dirfd(dir_ptr);
     
-    vector <string> pidFolders;
-
     // scan for pidFolders
+    vector <string> pidFolders;
     while( (dirent_ptr = readdir(dir_ptr))  != NULL){
 
         struct stat statbuf;
@@ -99,7 +145,7 @@ int scanProcess(){
     for (size_t i = 0; i < pidFolders.size(); i++){
 
         string fdPath = "/proc/" + pidFolders[i] + "/fd"; 
-        printf("\n%s\n", fdPath.c_str());
+        // printf("\n%s\n", fdPath.c_str());
         
         DIR *dir_ptr = opendir(fdPath.c_str());
         if(dir_ptr == NULL){
@@ -107,11 +153,10 @@ int scanProcess(){
             // exit(EXIT_FAILURE);
         }
 
-        struct dirent *dirent_ptr; readdir(dir_ptr);  
-
+        struct dirent *dirent_ptr;
         while( (dirent_ptr = readdir(dir_ptr)) != NULL) {
 
-            if(strcmp("..", dirent_ptr->d_name) != 0){
+            if( (strcmp("..", dirent_ptr->d_name) != 0) && (strcmp(".", dirent_ptr->d_name) != 0) ){
 
                 string linkPath = fdPath + "/" + dirent_ptr->d_name;
                 char buf[512];
@@ -133,50 +178,36 @@ int scanProcess(){
                     string sub1 = fdEntry.substr(0, fdEntry.find("]", 0));
                     string inode = sub1.substr(fdEntry.find("[", 0)+1);
 
-                    // cout << inode << endl;
-                    inodeEntry.inode = inode.c_str();
-                    inodeEntry.pid = pidFolders[i].c_str();
-                    string procName = parseProcName(inodeEntry);
-                    inodeEntry.procName = procName.c_str();
+                    // inodeEntry.inode = inode.c_str();
+                    // inodeEntry.pid = pidFolders[i].c_str();
+                    // string procName = parseProcName(inodeEntry);
+                    // inodeEntry.procName = procName.c_str();
 
-                    printf("%s %s %s\n", inodeEntry.pid, inodeEntry.inode, inodeEntry.procName);
+                    inodeEntry.inode = inode;
+                    inodeEntry.pid = pidFolders[i];
+                    inodeEntry.procName = parseProcName(inodeEntry);
 
+
+                    // printf("%s %s %s\n", inodeEntry.pid.c_str(), inodeEntry.inode.c_str(), inodeEntry.procName.c_str());
                     inodeList.push_back(inodeEntry);
                 }
             }
         }
         closedir(dir_ptr);
     }
-
     return 0;
 }
 
-int parseConnEntry(char *line){
+void outputResult(){
 
-    struct connInfo conn;
-    char* substr = strtok(line, " ");
-    
-    int idx = 0;
-    while(substr != NULL){
-
-        if(idx == 1){
-            conn.localAddr = substr;
+    for(size_t i = 0; i < connList.size(); i++){
+        for(size_t j = 0; j < inodeList.size(); j++){
+            
+            if (connList[i].inode == inodeList[j].inode){
+                printf("%d %s %s %s %s\n", connList[i].connType, connList[i].localAddr.c_str(), connList[i].remoteAddr.c_str(), inodeList[j].pid.c_str(), inodeList[j].procName.c_str());
+            }
         }
-        else if(idx == 2){
-            conn.remoteAddr = substr;
-        }
-        else if(idx == 9){
-            conn.inode = substr;
-        }
-
-        substr = strtok(NULL, " ");
-        idx++;
     }
-
-    printf("%s %s %s\n", conn.localAddr, conn.remoteAddr, conn.inode);
-    connList.push_back(conn);
-
-    return 0;
 }
 
 string parseProcName(struct procInfo inodeEntry){
@@ -189,26 +220,32 @@ string parseProcName(struct procInfo inodeEntry){
     
     fd = fopen(cmdFile.c_str(), "r");
     if (fd == NULL){
+        printf("[ERROR] failed to open %s\n", cmdFile.c_str());
+        return "";
         // exit(EXIT_FAILURE);
     }
 
     while(getline(&line, &n, fd) != -1){
         
-        // printf("%s\n", line);
         procName = line;
     }
 
+    fclose(fd);
     return procName;
 }
 
 int main(){
 
     readConnection("tcp", NET_FILE);
+
     scanProcess();
 
-    cout << inodeList.size() << endl;
-    // for (int i = 0; i < inodeList.size(); i++){
-    //     printf("%s %s %s\n", inodeList[i].pid, inodeList[i].inode, inodeList[i].procName);
+    outputResult();
+    // cout << inodeList.size() << endl;
+    // for (size_t i = 0; i < inodeList.size(); i++){
+    //     printf("%s %s %s\n", inodeList[i].pid.c_str(), inodeList[i].inode.c_str(), inodeList[i].procName.c_str());
+
+    //     // printf("%s %s\n", inodeList[i].pid, inodeList[i].inode);
     // }
 
     return 0;
