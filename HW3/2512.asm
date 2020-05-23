1686c88bbd80853773af784d13313a573c42d6dc
Alex Lin <h427m4nn.cs05@g2.nctu.edu.tw>
2020-05-23 19:47:08 +0800

    [HW3] Fix math3
    [Done]
    - Fix 1 problem
    	- math3:2512
    		Since var1 is unsigned, just fill higher bits with 0 to do the extension,
    		instead of calling cdq
    
    [TODO]
    - 6 problems left

--- HW3/2512.asm
+++ HW3/2512.asm
@@ -1,11 +1,11 @@
 mov eax, [0x600000] 
 mov ebx, 0x5
 mul ebx                ; edx:eax = var1 * 5
-; mov edx, 0x0           ; truncate the overflowed part (edx)
+mov edx, 0x0           ; sign extension for eax: fiil edx with 0 
+                       ; (because var1 is unsigned integer)
 
 mov ebx, [0x600004]     
 sub ebx, 0x3
-cdq                    ; sign extension to eax
 div ebx                ; eax = eax / (var2-3)
 
 mov [0x600008], eax
