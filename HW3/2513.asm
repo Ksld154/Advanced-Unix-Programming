b7139badc313b33823a029cbf2287583ae7ecba2
Alex Lin <h427m4nn.cs05@g2.nctu.edu.tw>
2020-05-23 19:28:03 +0800

    [HW3] Fix math3 to math5
    [Done]
    - Fix 3 problems:
    	math3:2512
    	math4:2513
    	math5:2514
    	Use sign extension to fix signed-integer overflow truncation
    
    [TODO]
    - 6 problems left

--- HW3/2513.asm
+++ HW3/2513.asm
@@ -2,19 +2,19 @@ mov eax, [0x600000]
 mov ebx, 0x5
 neg ebx                 ; ebx = -5
 imul ebx                ; edx:eax = var1 * (-5)
-; mov edx, 0x0            ; truncate the overflowed part (edx)
 mov [0x60000c], eax     ; var4 = var1 * (-5)
 
 mov eax, [0x600004]
 neg eax                 ; eax = -var2
+cdq                     ; sign extension:  edx:eax = eax
 mov ebx, [0x600008]
-idiv ebx                
+idiv ebx                ; eax = edx:eax / ebx  (eax = -var2 / var3)
 mov ecx, edx            ; ecx = -var2 % var3
-mov edx, 0x0
 
 mov eax, [0x60000c]
-idiv ecx
-mov edx, 0x0
-mov [0x60000c], eax
+cdq
+idiv ecx                ; eax = edx:eax / ecx       ( eax = (var1*(-5)) / (-var2%var3) )
+mov [0x60000c], eax     ; var4 =  (var1*(-5)) / (-var2%var3)
+
 
 done:
--- HW3/2514.asm
+++ HW3/2514.asm
@@ -2,13 +2,11 @@ mov eax, [0x600000]
 mov ecx, [0x600004] 
 neg ecx                 
 imul ecx                ; edx:eax = var1 * (-var2)
-mov edx, 0x0
-; mov edx, eax            ; edx = var1 * (-var2)
-
+cdq                     ; sign extension to eax
 
 mov ecx, [0x600008]
 sub ecx, ebx
-idiv ecx
+idiv ecx                ; edx:eax = (var1*(-var2)) / (var3–ebx)
 
 mov [0x600008], eax
 
