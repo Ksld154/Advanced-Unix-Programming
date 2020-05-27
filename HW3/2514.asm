937dd985d577ec45a7090024ecaa92865c842854
Alex Lin <h427m4nn.cs05@g2.nctu.edu.tw>
2020-05-27 11:14:44 +0800

    [HW3] remove useless files

--- HW3/2514.asm
+++ HW3/2514.asm
@@ -6,7 +6,7 @@ cdq                     ; sign extension to eax
 
 mov ecx, [0x600008]
 sub ecx, ebx
-idiv ecx                ; edx:eax = (var1*(-var2)) / (var3–ebx)
+idiv ecx                ; edx:eax = (var1*(-var2)) / (var3%ebx)
 
 mov [0x600008], eax
 
